7907cec374e571860158814851bd5df82af61551
Max Ovchinnikov <ovchinnicov132@gmail.com>
2022-03-30 21:08:06 +0300

    fix: updateDate

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
-let date = new Date();
-
-export const updateDate = () => {
-  const lastDate = date.setDate(date.getDate() + 7);
+export const updateDate = (summand) => {
+  let date = new Date();
+  const lastDate = date.setDate(date.getDate() + (!summand? 7 : summand ));
   date = new Date(lastDate);
   return date.toISOString().split("T")[0];
 };
@@ -14,16 +13,16 @@ export const arrTasks = [
   {
     _id: idRandom(),
     text: "Congratulate a friend on his birthday. Don't forget to buy him that cool mug I saw in the store the other day.",
-    date: updateDate(),
+    date: updateDate(1),
   },
   {
     _id: idRandom(),
     text: "Buy a house for your favorite cat.",
-    date: updateDate(),
+    date: updateDate(3),
   },
   {
     _id: idRandom(),
     text: "You should read that cool article about code optimization in React. Immediately apply knowledge in your pet project.",
-    date: updateDate(),
+    date: updateDate(5),
   },
 ];
